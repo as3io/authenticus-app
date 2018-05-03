38fd36fa2066ceceb08f7d8700d2d8cbc7035973
Jacob Bare <jacob@limit0.io>
2018-05-03 15:57:40 -0500

    Add initial app create

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('apps', function() {});
+  this.route('apps', function() {
+    this.route('create');
+  });
   this.route('users', function() {});
 });
 
