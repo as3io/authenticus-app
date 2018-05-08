1a0f19af384156b56663382e3ec93c8c994ab28f
Jacob Bare <jacob@limit0.io>
2018-05-08 17:09:49 -0500

    Add signup action

--- app/controllers/account/signup.js
+++ app/controllers/account/signup.js
@@ -1,24 +1,39 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
 
+import mutation from 'authenticus-app/gql/mutations/create-user';
+
 export default Controller.extend({
-  username: null,
-  password: null,
+  email: '',
+  password: '',
+  givenName: '',
+  familyName: '',
+
   errorMessage: null,
+
+  apollo: inject(),
   session: inject(),
 
   isLoading: false,
 
   actions: {
-    signup() {
-      // this.set('isLoading', true);
-      // this.set('errorMessage', null);
-      // const { username, password } = this.getProperties('username', 'password');
-      // this.get('session')
-      //   .authenticate('authenticator:application', username, password)
-      //   .catch((error) => this.set('errorMessage', error.errors.length ? error.errors[0].message : 'An unknown error has occurred.'))
-      //   .finally(() => this.set('isLoading', false))
-      // ;
-    }
-  }
+    async signup() {
+      this.set('isLoading', true);
+      this.set('errorMessage', null);
+      const payload = this.getProperties('email', 'password', 'givenName', 'familyName');
+      const variables = { input: { payload } };
+      const { email, password } = payload;
+      try {
+        await this.get('apollo').mutate({ mutation, variables }, 'createUser');
+        await this.get('session').authenticate('authenticator:application', email, password);
+      } catch (e) {
+        this.set('errorMessage', e.errors.length ? e.errors[0].message : 'An unknown error has occurred.');
+      } finally {
+        this.set('isLoading', false);
+      }
+    },
+    toggleShowPassword() {
+      this.toggleProperty('isPasswordShown');
+    },
+  },
 });
