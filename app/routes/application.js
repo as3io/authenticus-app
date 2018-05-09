f4b81db60dd6fe20eaf632a1bec5d3454be4272a
Jacob Bare <jacob@limit0.io>
2018-05-09 09:08:43 -0500

    Add main loading progress

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,44 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
+import ActionMixin from 'authenticus-app/mixins/action-mixin';
 
-export default Route.extend(ApplicationRouteMixin, {
+export default Route.extend(ApplicationRouteMixin, ActionMixin, {
   actions: {
+    showLoading() {
+      this.showLoading();
+    },
+
+    hideLoading() {
+      this.hideLoading();
+    },
+
     transitionTo(name) {
       return this.transitionTo(name);
+    },
+
+    scrollToTop() {
+      window.scrollTo(0, 0);
+    },
+
+    /**
+     *
+     * @param {*} transition
+     */
+    loading(transition) {
+      this.showLoading();
+      transition.finally(() => this.hideLoading());
+    },
+
+    /**
+     *
+     * @param {Error} e
+     */
+    error(e) {
+      if (this.get('graphErrors').isReady()) {
+        this.get('graphErrors').show(e);
+      } else {
+        this.intermediateTransitionTo('application_error', e);
+      }
     },
   },
 });
